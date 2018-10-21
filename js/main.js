9662809058f2426d483110137acd64deb52e26aa
AlexanderSwann <32579236+AlexanderSwann@users.noreply.github.com>
2018-10-21 12:12:02 -0400

    Update js/main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
-var request = new XMLHttpRequest();
-
-
-request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=2172797&APPID=d877a4ead94677246082ffd6ec22cb8a', true);
-//request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-
-request.onload = function () {
-
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-
+var weather;
+function setup() {
+  loadJSON('https://api.openweathermap.org/data/2.5/weather?id=4196508&APPID=d877a4ead94677246082ffd6ec22cb8a&units=metric', gotData, 'jsonp');
 }
 
-request.send();
+function gotData(data) {
+  weather = data;
+}
 
-var T = 21;
-var RH = 88;
-var p = 1021;
+var T = weather.main.temp;
+var RH = weather.main.humidity;
+var p = weather.main.pressure;
 
 var Rd = 287.058;
 var Rv = 461.495;
@@ -31,31 +25,3 @@ var pd = p - pv;
 var pp = ((pd * 100) / (Rd * (T + 273.15))) + ((pv * 100) / (Rv * (T + 273.15)));
 var ρ = ((100 * pd * md) + (100 * pv * mv)) / ((T + 273.15) * r);
 ρ = ρ.toFixed(4);
-//
-// // Create a request variable and assign a new XMLHttpRequest object to it.
-// var data = new XMLHttpRequest();
-//
-// // Open a new connection, using the GET request on the URL endpoint
-// request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=2172797&APPID=d877a4ead94677246082ffd6ec22cb8a', true);
-//
-// request.onload = function () {
-//   // Begin accessing JSON data here
-//   }
-// }
-//
-// // Send request
-// request.send();
-
-// var request = new XMLHttpRequest();
-//
-// request.open('GET', 'data.json', true);
-//
-// request.onload = function () {
-// 	// begin accessing JSON data here
-// 	var data = JSON.parse(this.response);
-//
-//   for (var i = 0; i < data.length; i++) {
-// 		console.log(data[i].name + ' is a ' + data[i].race + '.');
-// 	}
-// }
-// request.send();
